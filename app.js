760304d89852476a5c9d8744a077e6cae80fab9b
Raza Farooq <razaf88@gmail.com>
2015-06-06 19:15:28 -0400

    - Saving now to db

--- app.js
+++ app.js
@@ -1,48 +1,66 @@
-var express = require('express');
-var app = express();
+//var express = require('express');
+//var app = express();
 var cheerio = require('cheerio');
 var request = require('request');
-var sleep = require('sleep');
-
-var port = 3000;
-
-var base_url = 'http://www.canada411.ca/res/';
-
-var range = function (start, step) {
-	var array = [];
-
-	var length = step;
-
-	for (var i = 0; i <= length; i++) {
-		array[ i ] = start;
-		start++;
-	}
-
-	return array;
-};
-var phones = range(9057850000, 100);
-for (var i = 0; i < phones.length; i++) {
-	request(base_url + phones[ i ], function (error, response, body) {
-		if (!error && response.statusCode === 200) {
-			var $ = cheerio.load(body);
-			var json = { name: '', phone: '', address: '' };
-
-			$('#contact').filter(function () {
-				var data = $(this);
-
-				json.name = data.find('.c411ListedName').first().text();
-				json.phone = data.find('.c411Phone').first().text();
-				json.address = data.find('.c411Address').first().text().trim();
-			});
-
-			console.log('json: ', json);
-		} else {
-			
-		}
+var mongodb = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+mongoose.connect('mongodb://127.0.0.1:27017/directory');
+db.on('error', console.error);
+db.once('open', function() {
+	console.log('open');
+	var directorySchema = new mongoose.Schema({
+		name : String
+		, phone : String
+		, address: String
 	});
-}
+
+	var Directory = mongoose.model('Directory', directorySchema);
+
+
+	var base_url = 'http://www.canada411.ca/res/';
+
+	var range = function (start, step) {
+		var array = [];
+
+		for (var i = 0; i <= step; i++) {
+			array[ i ] = start;
+			start++;
+		}
+
+		return array;
+	};
+	var phones = range(9057850000, 100);
+	for (var i = 0; i < phones.length; i++) {
+		request(base_url + phones[ i ], function (error, response, body) {
+			if (!error && response.statusCode === 200) {
+				var $ = cheerio.load(body);
+
+				$('#contact').filter(function () {
+					var data = $(this);
+
+					var listing = new Directory({
+						name     : data.find('.c411ListedName').first().text()
+						, phone  : data.find('.c411Phone').first().text()
+						, address: data.find('.c411Address').first().text().trim()
+					});
+
+					listing.save(function (err, thor) {
+						if (err) return console.error(err);
+						console.dir(thor);
+					});
+				});
+			}
+		});
+	}
+});
+
+
+//var sleep = require('sleep');
+//var port = 3000;
+
 
 // START THE SERVER
 // =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+//app.listen(port);
+//console.log('Magic happens on port ' + port);
