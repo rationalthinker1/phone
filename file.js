0c073677b19b181296007a4851079416c2d9cd41
Raza Farooq <razaf88@gmail.com>
2015-07-08 02:24:03 -0400

    - Worked on file.js

--- file.js
+++ file.js
@@ -10,7 +10,7 @@
 //});
 // db.directories.update({}, { $set: { house_type: ''} }, false, true);
 /*
-* "BENJAMIN","LAU","23 BAYFIELD DR","RICHMOND HILL","ON","L4S2M5","9052370063","90729","40","M","Owner","English","Upscale","University/College","Mixed","<15","Large","After 1996","Mixed House Types"
+ * "BENJAMIN","LAU","23 BAYFIELD DR","RICHMOND HILL","ON","L4S2M5","9052370063","90729","40","M","Owner","English","Upscale","University/College","Mixed","<15","Large","After 1996","Mixed House Types"
  "J","HERRING","77722A LONDON RD RR 5","CLINTON","ON","N0M1L0","5194829514","55758","47","U","Owner","English","Middle","High School/Trade","Blue-Collar","<20","Large","Before 1946 & A","Single Detached Houses"
  "JIAN","ZHANG","11 GOLDFINCH CRT","NORTH YORK","ON","M2R2C2","4165196538","40917","44","U","Renter","Non-Official","Middle","University/College","Mixed","<6,25+","Medium","1961-80","High Rise Apartments"
  "D","BOOCOCK","1229 MARLBOROUGH CRT","OAKVILLE","ON","L6H3B6","9058448691","70878","41","U","Renter","Non-Official","Middle","University/College","Mixed","<6,25+","Medium","1961-80","High Rise Apartments"
@@ -24,47 +24,44 @@
  * */
 
 var fs       = require('fs');
-var csv      = require("fast-csv");
+var lazy     = require('lazy');
+var csv      = require('fast-csv');
 var _        = require('underscore');
 var argv     = require('minimist')(process.argv.slice(2));
 var cluster  = require('cluster');
 var mongoose = require('mongoose').connect('mongodb://127.0.0.1:27017/directory');
 var db       = mongoose.connection;
-
-var parse = require('csv-parse');
+var exec     = require('child_process').exec;
+var parse    = require('csv-parse');
 
 var Directory = require('./models/directory');
 var functions = require('./functions');
-
-var stream = fs.createReadStream('main.csv');
+var stream    = fs.createReadStream('main.csv');
 
 db.on('error', console.error);
 db.once('open', function () {
-    csv
-        .fromStream(stream, {ignoreEmpty : true})
+    csv.fromStream(stream, {ignoreEmpty: true})
         //.transform(function(data, next){
         //    MyModel.findById(data.id, next);
         //})
-        .on("data", function (data) {
-            var q = Directory.findOne({address: { $regex: new RegExp(data[2], 'i')}, house_type: { $eq: '' } } );
+        .on('data', function (data) {
+            //console.log(data[2], data[18]);
+            var address = data[2].trim();
+            var q       = Directory.findOne({address: {$regex: new RegExp(address, 'i')}});
             q.exec(function (err, record) {
                 var house_type = data[18].trim();
-                if(record != null && house_type != null) {
+                if (record != null && house_type != null && house_type != '') {
                     record.house_type = house_type;
                     record.save(function (err, element) {
                         if (err) {
                             return console.error(err);
                         }
+                        console.log(record);
                     });
-                    console.log(record);
                 }
             });
         })
-        .on("end", function () {
+        .on('end', function () {
             console.log("done");
         });
-
-
 });
-
-
---
+++ install_script.sh
@@ -0,0 +1,48 @@
+# update Nodejs
+npm cache clean -f
+npm install -g n
+n stable
+
+# install applications
+apt-get install -y p7zip-full
+apt-get install -y unzip
+apt-get install -y htop
+apt-get install -y git
+apt-get install -y vim
+
+# quietly add a user without password
+adduser --quiet --disabled-password -shell /bin/bash --home /home/raza --gecos "User" raza
+
+# set password
+echo "raza:cc" | chpasswd
+
+# add raza to sudoers
+sudo adduser raza sudo
+
+# login as raza
+su - raza
+
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+chmod -R 777 /usr/local/bin/
+
+# download bashrc and loads bashrc
+git clone https://github.com/rationalthinker1/bashrc.git
+cd bashrc/
+mv -f .* ~/
+cd
+source ~/.bashrc
+rm -rf bashrc/
+
+# download phone
+git clone https://github.com/rationalthinker1/phone.git
+
+# download dropbox uploader
+https://github.com/andreafabrizi/Dropbox-Uploader.git
+
+echo "cc" | sudo -S -v
+cd phone
+sudo npm install
+
+git config --global user.email "razaf88@gmail.com"
+git config --global user.name "Raza Farooq"
